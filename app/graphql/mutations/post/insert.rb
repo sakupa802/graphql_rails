3c1aaf41533540b379c8e8e6d5895dc00b398c91
akazaki.hiroki <akazaki.hiroki@a-tm.co.jp>
2018-12-19 22:49:48 +0900

    mod mutation

--- app/graphql/mutations/post/insert.rb
+++ app/graphql/mutations/post/insert.rb
@@ -1,20 +1,24 @@
 module Mutations::Post
   class Insert < GraphQL::Schema::Mutation
+    null false
+
     argument :title, String, required: true
     argument :content, String, required: true
 
-    def resolve(**arg)
-      'Hello World!'
-      # post = Post.new(
-      #   title: arg[:title],
-      #   content: arg[:content]
-      # )
+    field :post, Types::PostType, null: false
 
-      # if post.save
-      #   { post: post }
-      # else
-      #   raise GraphQL::ExecutionError, post.errors.full_messages.join(", ")
-      # end
+    def resolve(**arg)
+      #'Hello World!'
+      post = Post.new(
+        title: arg[:title],
+        content: arg[:content]
+      )
+
+      if post.save
+        { post: post }
+      else
+        raise GraphQL::ExecutionError, post.errors.full_messages.join(", ")
+      end
     end
   end
 end
